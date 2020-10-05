cbbcd007787d619aea48494de6e5348dea80f9fc
Carl Reed <carlreedw@gmail.com>
2020-10-05 15:53:49 -0500

    fix dashboard getMySiteMetricsData arg count Exception

--- dashboard.php
+++ dashboard.php
@@ -4,7 +4,7 @@ echo("PassItOn Dashboard");
 echo "<br>";
 echo "<h4>My Site Metrics</h4>";
 echo "<br>";
-$data = $module->getMySiteMetricsData();
+$data = $module->getMySiteMetricsData($module->getCurrentUserDAGName());
 echo ("PassItOn->getMySiteMetricsData()->site_name : <br><pre>" .print_r($data->site_name, true) . "<br></pre>");
 echo ("PassItOn->getMySiteMetricsData()->rows : <br><pre>" .print_r($data->rows, true) . "<br></pre>");
 ?>
