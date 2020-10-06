513ea28a1b4decbc4b4e15df5ac2854b3a3be0ff
Kyle McGuffin <kyle.mcguffin@vumc.org>
2020-10-06 08:37:51 -0400

    Add test for REDCap getUniqueGroupNames function

---
+++ tests/REDCapTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Vanderbilt\PassItOn;
+
+// For now, the path to "redcap_connect.php" on your system must be hard coded.
+require_once dirname(dirname(dirname(__DIR__))) . '/redcap_connect.php';
+require_once APP_PATH_DOCROOT . '/ExternalModules/tests/ModuleBaseTest.php';
+
+final class REDCapTest extends \ExternalModules\ModuleBaseTest
+{
+	function testGetDags(){
+		/** @var $module \Vanderbilt\PassItOn\PassItOn */
+		$module = $this->module;
+
+		$q = \ExternalModules\ExternalModules::getEnabledProjects($module->PREFIX);
+
+		if($row = db_fetch_assoc($q)) {
+			## Ensure that Project class exists and that it has a method named getUniqueGroupNames
+			$projectId = $row['project_id'];
+
+			$this->assertTrue(class_exists("Project"));
+
+			$Proj = new \Project($projectId);
+
+			$this->assertTrue(method_exists($Proj,"getUniqueGroupNames"));
+
+			## Ensure expected format of array return with numeric keys and string values
+			$dagNames = $Proj->getUniqueGroupNames();
+
+			$this->assertIsArray($dagNames);
+
+			$groupIds = array_keys($dagNames);
+
+			## Ensure return format
+			$this->assertIsNumeric(reset($groupIds));
+			$this->assertIsString(reset($dagNames));
+		}
+	}
+}
